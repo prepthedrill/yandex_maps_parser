ca1581ae854678e8342c7561e8b4d0d1535b4c02
Андрей Царегородцев <andreytsaregor@yandex.ru>
2023-05-10 22:06:44 +0300

    coordinate>json_category

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import time
 import json
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 NAME_INPUT_FILE = 'input.txt'
 
