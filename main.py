6ec5982c67cfa7905522fc190c80cdd13e286c71
Андрей Царегородцев <andreytsaregor@yandex.ru>
2023-05-10 21:40:12 +0300

    get_category_company

--- main.py
+++ main.py
@@ -10,12 +10,9 @@ browser.get('https://yandex.ru/maps/')
 
 time.sleep(1.5)
 
-# Ищем родительский класс для поискового окна
 search_box_root = browser.find_element(By.XPATH, "//div[@class='search-form-view__input']")
-# Ищем поисковое окно
 search_box = search_box_root.find_element(By.XPATH, "//input[@class='input__control _bold']")
-# Вводим текст запроса в поисковое окно
-search_box.send_keys("56.264527, 44.020764")
+search_box.send_keys("56.264527, 44.020764")  # Вводим текст запроса в окно поиска
 search_box.send_keys(Keys.RETURN)
 
 time.sleep(0.85)
@@ -28,13 +25,9 @@ button = button_root.find_element(By.XPATH, "//a[@class='tabs-select-view__label
 href = button.get_attribute('href') + 'inside/'
 
 browser.get(href)
-# businesses = browser.find_element(By.XPATH, "//div[@class='card-businesses-list__list']")
-companies = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='search-business-snippet-view']")))
-print(companies)
-for company in companies:
-    # name = company.find_element(By.XPATH, "//div[@class='search-business-snippet-view__title']")
-    print(company.text)
+companies_category = WebDriverWait(browser, 5).until(EC.presence_of_all_elements_located((By.XPATH, "//a[@class='search-business-snippet-view__category']")))
 
-time.sleep(2)
+for category in companies_category:
+    print(category.text)
 
 browser.quit()
