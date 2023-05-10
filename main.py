0594e7034b229b8b1458e5ed42ceed02dbea35c9
Андрей Царегородцев <andreytsaregor@yandex.ru>
2023-05-10 21:35:18 +0300

    get_all_text

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 browser = webdriver.Chrome()
@@ -19,15 +21,19 @@ search_box.send_keys(Keys.RETURN)
 time.sleep(0.85)
 
 button_root = browser.find_element(By.XPATH, "//div[@class='carousel__content']")
-print('Успех')
 time.sleep(0.85)
 
 button = button_root.find_element(By.XPATH, "//a[@class='tabs-select-view__label']")
 
 href = button.get_attribute('href') + 'inside/'
-href.c
-print(href)
-print('Ура')
+
+browser.get(href)
+# businesses = browser.find_element(By.XPATH, "//div[@class='card-businesses-list__list']")
+companies = WebDriverWait(browser, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='search-business-snippet-view']")))
+print(companies)
+for company in companies:
+    # name = company.find_element(By.XPATH, "//div[@class='search-business-snippet-view__title']")
+    print(company.text)
 
 time.sleep(2)
 
